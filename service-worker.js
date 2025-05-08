e7a1135f9606077d71578eb2b96b42d5265374ff
Cath0867 <cathsantossilveira@gmail.com>
2025-05-08 12:09:27 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,23 @@
-const CACHE_NAME = 'flashcards-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/flashcards.js',
-    '/flashcards.json',
-    '/images/icon-192x192.png',
-    '/images/icon-512x512.png'
-];
-
-// Instalando o service worker e armazenando arquivos no cache
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Service Worker: Cache inicializado');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open('flashcards-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/flashcards.json',
+                '/style.css',
+                '/flashcards.js',
+                '/manifest.json'
+            ]);
+        })
     );
 });
 
-// Quando o usuário acessar, tente obter o recurso do cache
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request); // Retorna o cache ou faz a requisição
-            })
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
     );
 });
+
