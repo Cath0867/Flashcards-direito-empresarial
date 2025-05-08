acb99e09b9f06be5adcff6ecd5e6cee336163f4a
Cath0867 <cathsantossilveira@gmail.com>
2025-05-08 12:02:38 -0300

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,17 +1,29 @@
-fetch("flashcards.json")
-  .then((response) => response.json())
-  .then((cards) => {
-    const container = document.getElementById("flashcard-container");
+// Carregar o JSON com as perguntas e respostas
+fetch('flashcards.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('flashcards-container');
 
-    cards.forEach((card) => {
-      const cardElement = document.createElement("div");
-      cardElement.className = "card";
-      cardElement.innerHTML = `
-        <div class="card-inner">
-          <div class="card-front">${card.pergunta}</div>
-          <div class="card-back">${card.resposta}</div>
-        </div>
-      `;
-      container.appendChild(cardElement);
-    });
-  });
+        data.forEach(card => {
+            const flashcard = document.createElement('div');
+            flashcard.classList.add('flashcard');
+            
+            const question = document.createElement('div');
+            question.classList.add('question');
+            question.innerText = card.pergunta;
+            
+            const answer = document.createElement('div');
+            answer.classList.add('answer');
+            answer.innerText = card.resposta;
+
+            flashcard.appendChild(question);
+            flashcard.appendChild(answer);
+
+            flashcard.addEventListener('click', () => {
+                flashcard.classList.toggle('flipped');
+            });
+
+            container.appendChild(flashcard);
+        });
+    })
+    .catch(error => console.log('Erro ao carregar os flashcards:', error));
