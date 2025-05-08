a25d633bc04d2425e6b78bdc65190b707319f6c2
Cath0867 <cathsantossilveira@gmail.com>
2025-05-08 13:00:30 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,24 @@
-const CACHE_NAME = 'flashcards-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/flashcards.js',
-    '/manifest.json'
-];
-
-// Instala o service worker
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('flashcards-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/flashcards.js',
+        '/flashcards.json',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
 });
 
-// Ativa o novo service worker e limpa caches antigos
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames =>
-            Promise.all(
-                cacheNames.map(name => {
-                    if (name !== CACHE_NAME) return caches.delete(name);
-                })
-            )
-        )
-    );
-});
-
-// Intercepta requisições e responde com cache se disponível
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
